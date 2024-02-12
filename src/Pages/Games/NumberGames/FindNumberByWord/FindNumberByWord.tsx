33b9948d802ba94b278e644bcd915631921e683a
Deiv-Dev <nomaxas@gmail.com>
2024-02-12 11:16:05 +0000

    added feedback message component to notifie wrong or correct press

--- src/Pages/Games/NumberGames/FindNumberByWord/FindNumberByWord.tsx
+++ src/Pages/Games/NumberGames/FindNumberByWord/FindNumberByWord.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-
 import CardComponents from "../../../../Components/GameComponents/CardComponents/CardComponents";
 import {
   numbersToFindInWords,
@@ -12,6 +11,7 @@ import {
   shuffleArray,
 } from "../../../../Helpers/ArrayHelpers";
 import { getRandomLightColor } from "../../../../Helpers/RandomColorGenerator";
+import FeedbackMessageComponent from "../../../../Components/GameComponents/FeedbackMessageComponent/FeedbackMessageComponent";
 
 function FindNumberByWord() {
   const [currentNumberToFind, setCurrentNumberToFind] = useState<number>(0);
@@ -22,6 +22,7 @@ function FindNumberByWord() {
   const [cardsBackgroundColors, setCardsBackgroundColors] = useState<string[]>(
     []
   );
+  const [isAnswerCorrect, setIsAnswerCorrect] = useState<boolean | null>(null);
 
   useEffect(() => {
     setShuffledNumbersToShowOnCards(() => {
@@ -42,25 +43,40 @@ function FindNumberByWord() {
 
   const handleCardClick = (card: number): void => {
     if (currentNumberToFind + 1 === card) {
-      setCurrentNumberToFind((prevNumber) => prevNumber + 1);
+      setIsAnswerCorrect(true);
+      setTimeout(() => {
+        setCurrentNumberToFind((prevNumber) => prevNumber + 1);
+        setIsAnswerCorrect(null);
+      }, 500);
+    } else {
+      setIsAnswerCorrect(false);
+      setTimeout(() => {
+        setIsAnswerCorrect(null);
+      }, 500);
     }
   };
 
   return (
-    <Container>
-      <Row>
-        <Col className="card" style={{ backgroundColor: "rgb(151, 212, 159)" }}>
-          <p className="card__text">
-            {numbersToFindInWords[currentNumberToFind]}
-          </p>
-        </Col>
-      </Row>
-      <CardComponents
-        chunkedArray={chunkedNumbersToShowOnCards}
-        cardsBackgroundColors={cardsBackgroundColors}
-        handleCardClick={handleCardClick}
-      />
-    </Container>
+    <>
+      <Container>
+        <Row>
+          <Col
+            className="card"
+            style={{ backgroundColor: "rgb(151, 212, 159)" }}
+          >
+            <p className="card__text">
+              {numbersToFindInWords[currentNumberToFind]}
+            </p>
+          </Col>
+        </Row>
+        <CardComponents
+          chunkedArray={chunkedNumbersToShowOnCards}
+          cardsBackgroundColors={cardsBackgroundColors}
+          handleCardClick={handleCardClick}
+        />
+      </Container>
+      <FeedbackMessageComponent isCorrect={isAnswerCorrect} />
+    </>
   );
 }
 
